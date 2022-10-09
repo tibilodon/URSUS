7df61caec465db26f77604609e47b39dc5f43c52
Vigh Tibor <tibilodondev@gmail.com>
2022-10-09 08:23:23 +0200

    recipesController.js is next, going through server.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,33 @@
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import BadRequestError from "../errors/badRequest.js";
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("Adatok megadása szükséges");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("E-mail cím már használatban");
+  }
+  const user = await User.create({ name, email, password });
+  const toke = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
+};
+const login = async (req, res) => {
+  res.send("login user");
+};
+const updateUser = async (req, res) => {
+  res.send("update user");
+};
+export { register, login, updateUser };
---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+import "express-async-errors";
+
+//db and authUser
+import connectDB from "./db/connect.js";
+
+//routes
